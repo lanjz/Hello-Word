6eeea935e9ab3a06a314f4604b6f111b1b72e711
lanjz <156081289@qq.com>
2020-06-09 21:29:18 +0800

    vim 快捷键

--- Front_End/前端知识体系/HTML/HTML5/demo/WebWoker/demo/worker.js
+++ Front_End/前端知识体系/HTML/HTML5/demo/WebWoker/demo/worker.js
@@ -5,14 +5,14 @@ console.log('delf', self.addEventListener, this)
     doWorker(e)
 }*/
 function doWorker(e) {
-    if(e.data === 'start') {
+   /* if(e.data === 'start') {
         let count = 0
         setInterval(() => {
             postMessage(count++)
         }, 1000)
     } else if(e.data === 'end') {
         close()
-    }
+    }*/
 }
 self.addEventListener('message', function (e) {
     console.log('addEventListener: Message received from main script', e);
