182df6d89da468a3f1e15074c2642b85e083543f
lanjz <156081289@qq.com>
2020-11-29 23:31:00 +0800

    svg

---
+++ blog/docs/.vuepress/components/data.js
@@ -0,0 +1,236 @@
+const tree2 = [
+  {
+    name: '1-1-1',
+    children: [
+      {
+        name: '1-1-1-1',
+        children: [
+          {
+            name: 'a-1-3-1',
+          },
+          {
+            name: 'a-1-3-2'
+          },  {
+            name: 'a-1-3-4',
+            type: 'text'
+          }
+        ]
+      },
+      {
+        name: '1-1-1-2'
+      }
+    ]
+  },
+  {
+    name: '1-1-2',
+    children: [
+      {
+        name: '1-2-1',
+        type: 'text'
+      },
+      {
+        name: '1-2-2',
+        type: 'text'
+      },
+      {
+        name: '1-2-3',
+        type: 'text'
+      },
+      {
+        name: '1-2-4'
+      }
+    ]
+  },
+  
+  {
+    name: '1-1-3',
+    children: [
+      {
+        name: '1-1-3-1',
+        children: [
+          {
+            name: '1-2-1',
+            
+          },
+          {
+            name: 'a-1-3-3',
+            children: [
+              {
+                name: 'a-1-3-1',
+                children: [
+                  {
+                    name: 'a-1-3-1',
+                  },
+                  {
+                    name: 'a-1-3-2'
+                  },
+                  {
+                    name: 'a-1-3-3'
+                  },
+                  {
+                    name: 'a-1-3-4'
+                  }
+                ]
+              },
+              {
+                name: 'a-1-3-2'
+              },
+              {
+                name: 'a-1-3-3'
+              },
+              {
+                name: 'a-1-3-4'
+              }
+            ]
+          },
+          {
+            name: '1-2-2'
+          },
+          {
+            name: '1-2-3'
+          },
+          {
+            name: '1-2-4'
+          }
+        ]
+      },
+      {
+        name: '1-1-3-2'
+      }
+    ]
+  },
+  {
+    name: '1-1-4',
+    children: [
+      {
+        name: '1-1-3-1',
+        children: [
+          {
+            name: '1-2-1',
+            
+          },
+          {
+            name: 'a-1-3-3',
+            children: [
+              {
+                name: 'a-1-3-1',
+                children: [
+                  {
+                    name: 'a-1-3-1',
+                  },
+                  {
+                    name: 'a-1-3-2'
+                  },
+                  {
+                    name: 'a-1-3-3'
+                  },
+                  {
+                    name: 'a-1-3-4'
+                  }
+                ]
+              },
+              {
+                name: 'a-1-3-2'
+              },
+              {
+                name: 'a-1-3-3'
+              },
+              {
+                name: 'a-1-3-4'
+              }
+            ]
+          },
+          {
+            name: '1-2-2'
+          },
+          {
+            name: '1-2-3'
+          },
+          {
+            name: '1-2-4'
+          }
+        ]
+      },
+      {
+        name: '1-1-3-2'
+      }
+    ]
+  }
+]
+const tree = [
+  {
+    name: '1',
+    type: 'dir',
+    children: [
+      {
+        name: '1-1',
+        children: [
+          {
+            name: '1-1-1',
+          },
+          {
+            name: '1-1-2'
+          }
+        ]
+      },
+      {
+        name: '1-2',
+        children: [
+          {
+            name: '1-2-1',
+          },
+          {
+            name: '1-2-2'
+          },
+          {
+            name: '1-2-3'
+          },
+          {
+            name: '1-2-4'
+          }
+        ]
+      },
+      {
+        name: '1-3',
+        children: [
+          {
+            name: '1-3-1',
+          },
+          {
+            name: '1-3-2'
+          },
+          {
+            name: '1-3-3'
+          },
+          {
+            name: '1-3-4'
+          }
+        ]
+      }
+    ]
+  }
+]
+const root = {
+  name: '前端知识',
+  children: [...tree2, ...tree]
+}
+export const min = {
+  name: '3333',
+  children: [
+    {
+      name: 1344,
+      children: [
+        {
+          name: 1344,
+          
+        },
+        {
+          name: 3455
+        }
+      ]
+    },
+    {
+      name: 3455
+    }
+  ]
+}
