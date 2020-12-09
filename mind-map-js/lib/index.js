dd970b0fda330d8b6015bfa0322ed710e15c3420
lanjz <156081289@qq.com>
2020-12-10 00:31:38 +0800

    整理

---
+++ mind-map-js/lib/index.js
@@ -0,0 +1,12 @@
+function component() {
+  var element = document.createElement('div');
+  element.innerHTML = 'Hello';
+  const abc = () => {
+    console.log('箭头函数')
+  }
+  new Promise(() => {})
+  var b = [1, 2, 3].includes(1);
+  return element;
+}
+
+document.body.appendChild(component());
---
+++ mind-map-js/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path')
+const config = {
+  mode: 'development',
+  entry: {
+    index: './lib/index.js',
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js/,
+        use: [
+          'babel-loader'
+        ]
+      }
+    
+    ]
+  }
+}
+module.exports = config
