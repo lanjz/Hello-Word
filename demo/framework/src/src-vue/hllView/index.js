80475d4adf8d6db30bb59b450abda8269ae77872
jiangzhou.lan <jiangzhou.lan@huolala.cn>
2021-01-29 14:09:58 +0800

    优化

--- demo/framework/src/src-vue/hllView/index.js
+++ demo/framework/src/src-vue/hllView/index.js
@@ -2,12 +2,31 @@ import emitter from './mixins/emitter'
 import IndexList from './components/indexList/IndexList'
 import IndexListContent from './components/indexList/IndexListContent'
 import IndexAnchor from './components/indexList/IndexAnchor'
-import Modal from './hllComponents/Modal/Modal'
+import Fold from './components/fold/Fold'
+import Modal from './hllComponents/modal/Modal'
+import Neat from './hllComponents/neat/Neat'
+import NeatItem from './hllComponents/neat/NeatItem'
+// import HllUpload from './hllComponents/upload/HllUpload'
+// import PreviewFile from './hllComponents/preivew/PreviewFile'
+// import PreviewImage from './hllComponents/preivew/PreviewImage'
+import Form from './hllComponents/form/Form'
+// api 组件
+// import PreviewFileApi from './hllComponents/preivew/api'
 const components = [
   IndexList,
   IndexListContent,
   IndexAnchor,
-  Modal
+  Modal,
+  Neat,
+  NeatItem,
+  // HllUpload,
+  // PreviewFile,
+  // PreviewImage,
+  Form,
+  Fold,
+]
+const apiCom = [
+  // PreviewFileApi
 ]
 export default {
   install(Vue){
@@ -16,6 +35,9 @@ export default {
     })
     components.forEach(component => {
       Vue.component(component.name, component);
+    });
+    apiCom.forEach(componentApi => {
+      Vue.prototype['$'+componentApi.alias] = componentApi
     });
   }
 }
--- demo/framework/src/src-vue/router/component.js
+++ demo/framework/src/src-vue/router/component.js
@@ -1,6 +1,7 @@
 
 import IndexList from '../hllView/components/indexList'
 import Dialog from '../hllView/hllComponents/Modal'
+import Form from '../page/com-demo/form/Index'
 const diff = { template: '<div>diff</div>' }
 
 /*要注意，以 / 开头的嵌套路径会被当作根路径。 这让你充分的使用嵌套组件而无须设置嵌套的路径*/
@@ -18,6 +19,11 @@ export default {
             name: '弹层',
             path: 'dialog',
             component: Dialog
+        },
+        {
+            name: '表单',
+            path: 'form',
+            component: Form
         },
         {
             name: '首页3',
