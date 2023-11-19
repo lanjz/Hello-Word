9ddcba80f12a15f4bf88cf52b3e167f2180a728a
156081289@qq.com <156081289@qq.com>
2023-11-19 16:18:55 +0800

    feat: delete unuse

--- demo/Node/process.js
+++
@@ -1,9 +0,0 @@
-const fork = require('child_process').fork;
-return
-const server = require('net').createServer();
-server.listen(3000);
-const worker = fork('worker.js');
-
-worker.send('server', server);
-console.log('worker process created, pid: %s ppid: %s', worker.pid, process.pid);
-// process.exit(0); // 创建子进程之后，主进程退出，此时创建的 worker 进程会成为孤儿进程
