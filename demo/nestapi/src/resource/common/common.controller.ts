6a6c14dbd4b34d8d2b73ef6f81666a3b267e6b6d
156081289@qq.com <156081289@qq.com>
2024-01-14 18:11:02 +0800

    feat: 上传文件格式优化

--- demo/nestapi/src/resource/common/common.controller.ts
+++ demo/nestapi/src/resource/common/common.controller.ts
@@ -6,14 +6,13 @@ import {
   Body,
   ParseFilePipeBuilder,
   ParseFilePipe,
-  UploadedFiles,
+  UploadedFiles, BadRequestException,
 } from '@nestjs/common';
 import * as multer from 'multer';
 import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 import * as path from 'path';
 import * as fs from 'fs';
 import { v4 as uuidv4 } from 'uuid';
-
 function getFileName(filename) {
   const index = filename.lastIndexOf('.');
   return filename.substring(0, index);
@@ -21,7 +20,6 @@ function getFileName(filename) {
 const storage = multer.diskStorage({
   destination: './uploads',
   filename: (req, file, cb) => {
-    console.log('file', file);
     const uniqueFilename = `${getFileName(
       file.originalname,
     )}-${uuidv4()}${path.extname(file.originalname)}`;
@@ -33,27 +31,36 @@ const upload = multer({ storage });
 
 @Controller('common')
 export class CommonController {
-  @UseInterceptors(FileInterceptor('file', { storage }))
+  //  获取单个文件
   @Post('file')
+  @UseInterceptors(FileInterceptor('file', { storage }))
   uploadFile(@Body() body: any, @UploadedFile() file: Express.Multer.File) {
-    console.log('file', file);
     return {
-      body,
+      ...body,
       filePath: file ? `/uploads/${file.filename}` : null,
     };
   }
-
+  // 添加格式校验的上传
   @UseInterceptors(FileInterceptor('file', { storage }))
-  @Post('file/pass-validation')
+  @Post('file-validation')
   uploadFileAndPassValidation(
     @Body() body: any,
     @UploadedFile(
       new ParseFilePipeBuilder()
         .addFileTypeValidator({
-          fileType: 'json',
+          fileType: 'pdf',
         })
         .build({
-          fileIsRequired: false,
+          fileIsRequired: true, // 文件不能为空
+          exceptionFactory(err){
+            if(err.indexOf('Validation failed') > -1) {
+              return new BadRequestException('文件格式不正常')
+            }
+            let  map = {
+              'File is required': '文件不能为空'
+            }
+            return new BadRequestException(map[err])
+          }
         }),
     )
     file?: Express.Multer.File,
@@ -63,33 +70,13 @@ export class CommonController {
       file: file ? file.path : '',
     };
   }
-
-  @UseInterceptors(FileInterceptor('file', { storage }))
-  @Post('file/fail-validation')
-  uploadFileAndFailValidation(
-    @Body() body: any,
-    @UploadedFile(
-      new ParseFilePipeBuilder()
-        .addFileTypeValidator({
-          fileType: 'jpg',
-        })
-        .build(),
-    )
-    file: Express.Multer.File,
-  ) {
-    return {
-      body,
-      file: file.buffer.toString(),
-    };
-  }
-
   @UseInterceptors(FilesInterceptor('files'))
   @Post('files')
   uploadMultipleFiles(
     @Body() body: any,
     @UploadedFiles(
       new ParseFilePipeBuilder()
-        .addFileTypeValidator({
+        .addFileTypeValidator({ // 添加格式校验
           fileType: 'jpg',
         })
         .build(),
@@ -97,7 +84,6 @@ export class CommonController {
     files: Array<Express.Multer.File>,
   ) {
     console.log('files', files);
-
     const filePaths = [];
     for (const file of files) {
       const uniqueFilename = `${getFileName(
@@ -106,7 +92,6 @@ export class CommonController {
       fs.writeFileSync(path.join('./uploads', uniqueFilename), file.buffer);
       filePaths.push(`/uploads/${uniqueFilename}`);
     }
-
     return {
       body,
       filePaths,
