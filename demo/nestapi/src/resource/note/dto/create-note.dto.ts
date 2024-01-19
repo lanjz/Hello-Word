c495c155bdb2c039b09624190296f71f306f65bb
156081289@qq.com <156081289@qq.com>
2024-01-20 00:21:59 +0800

    feat: 添加note表  以后使用

--- demo/nestapi/src/resource/note/dto/create-note.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsNotEmpty } from 'class-validator'
-
-export class CreateNoteDto {
-	@IsNotEmpty({message: '标题不能为空'})
-	title: string;
-
-	@IsNotEmpty({message: '内容不能为空'})
-	content: string;
-}
--- demo/nestapi/src/resource/note/note.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { NoteService } from './note.service';
-import { NoteController } from './note.controller';
-
-@Module({
-  controllers: [NoteController],
-  providers: [NoteService],
-})
-export class NoteModule {}
