70891c92d2cc8f4a026a641099a51b4225f47580
156081289@qq.com <156081289@qq.com>
2024-01-20 00:21:26 +0800

    feat: 优化角色表

--- demo/nestapi/src/resource/role/entities/role.entity.ts
+++ demo/nestapi/src/resource/role/entities/role.entity.ts
@@ -1,6 +1,5 @@
 import { CommonEntity } from '@/extends/common.entity';
-import { Column, Entity, ManyToMany } from 'typeorm'
-import UserEntity from '../../user/entities/user.entity'
+import { Column, Entity } from 'typeorm'
 import { RoleEnum } from '@/utils/const'
 
 @Entity('role')
@@ -10,7 +9,4 @@ export default class Role extends CommonEntity{
 
 	@Column({name: 'role_name',  unique: true})
 	roleName: string;
-
-	@ManyToMany(() => UserEntity, (user) => user.roles)
-	users: UserEntity[]
 }
